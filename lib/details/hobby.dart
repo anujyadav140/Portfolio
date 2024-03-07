06fbdeb899cc1d60dfdc9742532a53ca265c4397
Anuj Yadav <anujyadav140@gmail.com>
2024-03-07 15:36:50 +0530

    final fix

--- lib/details/hobby.dart
+++ lib/details/hobby.dart
@@ -26,18 +26,18 @@ class _HobbiesState extends State<Hobbies> {
                 "",
                 _buildStaggeredGridView(
                   Colors.white,
-                  'https://cdn.discordapp.com/attachments/889189690052325396/1173583893467779184/Screenshot_2023-11-13-16-29-43-114_com.instagram.android-edit.jpg?ex=65e5af2c&is=65d33a2c&hm=bc5fbd24ec5e85db405046fdcef557ea0d7b024dffc021298fdbec07559a17b2&',
-                  'https://cdn.discordapp.com/attachments/889189690052325396/1173583932164415548/Screenshot_2023-11-13-16-31-23-104_com.instagram.android-edit.jpg?ex=65e5af35&is=65d33a35&hm=b945a9dac35980abc2e977a582270e7326798b9acf57994369147cfdc393dddc&',
-                  'https://cdn.discordapp.com/attachments/889189690052325396/1173583907124412486/Screenshot_2023-11-13-16-30-16-598_com.instagram.android-edit.jpg?ex=65e5af2f&is=65d33a2f&hm=5c32f9255c7f889a9539dc725866f2b4a1bb2bad80c5781de35e40eeec55f97c&',
-                  'https://cdn.discordapp.com/attachments/889189690052325396/1173583921737388032/Screenshot_2023-11-13-16-30-54-817_com.instagram.android-edit.jpg?ex=65e5af33&is=65d33a33&hm=f6ccd11650dc73a197b2856af7d8707e097c153939d8e65a2da2b9836354de9b&',
-                  'https://cdn.discordapp.com/attachments/889189690052325396/1173627358436151327/Anuj1_1.gif?ex=65e5d7a7&is=65d362a7&hm=57e6c908aa94226f21fc4bd35a99b8078849be7b867c9373974cdac52945a04f&',
-                  'https://cdn.discordapp.com/attachments/889189690052325396/1173635521193451630/Screenshot_2023-11-13-20-17-22-131_com.instagram.android-edit.jpg?ex=65e5df41&is=65d36a41&hm=569d884e52e7a347a24f93bc09add7bda76b653f696cb03a49aedcb0a4e0bb8c&',
-                  'https://cdn.discordapp.com/attachments/1178553564235321384/1178601624269234186/Screenshot_2023-11-27_131137.png?ex=65e57b4c&is=65d3064c&hm=51656534679389b0a6d66a3ade0d6f03ad75e61f2b5918092e45591260e0c250&',
-                  'https://cdn.discordapp.com/attachments/1178553564235321384/1178601644892626964/Screenshot_2023-11-27_124725.png?ex=65e57b51&is=65d30651&hm=52edb7715495f5b86eaca46c8b4707d5af39b2b951f04ea9c6da3eaa0b72b35d&',
-                  'https://cdn.discordapp.com/attachments/1178553564235321384/1178702339935314030/photo1701094709.jpeg?ex=65e5d919&is=65d36419&hm=d81d3eb4f0f5eab18cc74cf1be17b64384eb04989756bb70d7c952ccc8eab335&',
-                  'https://cdn.discordapp.com/attachments/1178553564235321384/1178921833626611722/photo1701094823.jpeg?ex=65e6a584&is=65d43084&hm=64c9d2040da283079be630eebc27b82027c9299d6d72fe7b45c2f4a46493aa2e&',
-                  'https://cdn.discordapp.com/attachments/1178553564235321384/1187054705818480842/533b09af-871c-47bf-bd47-e52c08fbca97.jpeg?ex=65e88c58&is=65d61758&hm=0ef16de393fee8603180efdeff8b78c54313fccf0b30dc4943cb0ef571a76ce5&',
-                  'https://cdn.discordapp.com/attachments/1178553564235321384/1187424005272109086/6828fbdc-c439-4c39-8ea1-999c6db36bca.jpeg?ex=65e9e448&is=65d76f48&hm=92dd73c7ef83d16bf2c751710e79939d52c2102f275e159133103a69504f6181&',
+                  'assets/demos/hobby/boy.png',
+                  'assets/demos/hobby/good.png',
+                  'assets/demos/hobby/dan.png',
+                  'assets/demos/hobby/baud.png',
+                  'assets/demos/hobby/self.gif',
+                  'assets/demos/hobby/gaddis.png',
+                  'assets/demos/hobby/poetry-1.png',
+                  'assets/demos/hobby/poetry-2.png',
+                  'assets/demos/hobby/jap.png',
+                  'assets/demos/hobby/angel.png',
+                  'assets/demos/hobby/net.png',
+                  'assets/demos/hobby/anuj.png',
                 )),
           ),
         ],
@@ -89,7 +89,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 2,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link1,
             fit: BoxFit.fitWidth,
           ),
@@ -101,7 +101,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1.25,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link5,
             fit: BoxFit.fitHeight,
           ),
@@ -113,7 +113,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1.25,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link6,
             fit: BoxFit.fitWidth,
           ),
@@ -125,7 +125,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link2,
             fit: BoxFit.fitWidth,
           ),
@@ -137,7 +137,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link3,
             fit: BoxFit.fitHeight,
           ),
@@ -149,7 +149,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1,
         child: Container(
           color: color1,
-          child: Image.network(link4),
+          child: Image.asset(link4),
         ),
       ),
       //living room
@@ -173,7 +173,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 2,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link7,
             fit: BoxFit.fitHeight,
           ),
@@ -185,7 +185,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 2,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link8,
             fit: BoxFit.fitHeight,
           ),
@@ -197,7 +197,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1.5,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link9,
             fit: BoxFit.fitHeight,
           ),
@@ -209,7 +209,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1.5,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link10,
             fit: BoxFit.cover,
           ),
@@ -221,7 +221,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1.5,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link11,
             fit: BoxFit.cover,
           ),
@@ -234,7 +234,7 @@ Widget _buildStaggeredGridView(
         mainAxisCellCount: 1.5,
         child: Container(
           color: color1,
-          child: Image.network(
+          child: Image.asset(
             link12,
             fit: BoxFit.fitHeight,
           ),
